040f77cb5d19efdc79d69398233ad71ecbca5c74
Priyanka-K-A <priyankaashokan0607@gmail.com>
2023-03-17 18:54:56 +0530

    init

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './navbar.css'
 import {MdOutlineTravelExplore} from 'react-icons/md'
 import {AiFillCloseCircle} from 'react-icons/ai'
