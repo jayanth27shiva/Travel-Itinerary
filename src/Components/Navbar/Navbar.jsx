7f41a2d60a03f558f21aea76375f0ca7ea6616e5
jayanth27shiva <jayanth27shiva@gmail.com>
2023-03-30 11:44:24 +0530

    init

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                 <div className="logoDiv">
                     <a href="#" className="logo flex" >
                         <h1><MdOutlineTravelExplore 
-                        className="icon"/>  Travel.</h1>
+                        className="icon"/>  Travel Itinerary</h1>
                     </a>
                 </div>
                 
